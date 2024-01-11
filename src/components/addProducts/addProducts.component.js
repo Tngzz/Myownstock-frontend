4d9b4f991ae78fd5a9083a50c7a2565a6a3835ae
Tanguy Dorval <dorvaltanguy@gmail.com>
2024-01-11 13:29:03 +0100

    correction bug form + css form

--- src/components/addProducts/addProducts.component.js
+++ src/components/addProducts/addProducts.component.js
@@ -2,17 +2,14 @@ import { Form } from './form'
 
 export class addProductsComponent {
     #title = 'Add Products'
-    #template = ``
+    #template = `Add products to your stock !`
+    #form = new Form()
     
     load() {
         const title = document.querySelector('h1')
         title.innerHTML = this.#title
         const app = document.querySelector('[app]')
         app.innerHTML = this.#template
-        this.#onInit()
-    }
-
-    async #onInit () {
-        this.#template += `<div class="container">`
-    }
+        this.#form.displayForm()
+    }  
 }
--- src/components/addProducts/form.js
+++ src/components/addProducts/form.js
@@ -1,17 +1,18 @@
+import './form.css'
 
 export class Form {
 
     #template = `
     <div class="container">
-        <form class = Form">
-            <label for="fname">id (*)</label>
-            <input type="text" id="fname" name="id" placeholder="id">
+        <form class = form" onSubmit = "return(ValidationForm)">
+            <label for="fname">id</label>
+            <input type="text" id="fname" name="id" placeholder="id" required>
 
-            <label for="lname">Label(*)</label>
-            <input type="text" id="lname" name="lastname" placeholder="Label">
+            <label for="lname">Label</label>
+            <input type="text" id="lname" name="lastname" placeholder="Label" required>
 
             <label for="quantite" placeholder= "Stock :"</label>
-            <input type="number" id="Stock" name="Stock" min="0" max="100" step="1">
+            <input type="number" id="Stock" name="Stock" min="0" max="100" step="1" required>
 
             <input type="submit" value="Add">
         </form>
@@ -22,13 +23,29 @@ export class Form {
     }
 
     displayForm() {
-        const formElement = document.createElement('div');
-        formElement.innerHTML = this.render();
-
-        document.body.appendChild(formElement);
+        const formElement = document.createElement('div')
+        formElement.innerHTML = this.render()
+        document.body.appendChild(formElement)
     }
 }
 
-const myForm = new Form()
-myForm.displayForm()
+// function ValidationForm () {
+//         var id = document.getElementById("fname").value
+//         // var name = document.getElementById("lname").value
+//         // var stock = document.getElementById("Stock").value
 
+//         var idError = document.getElementById("idError")
+//         // var nameError = document.getElementById("nameError")
+//         // var stockError = document.getElementById("stockError")
+
+//         // idError.innerHTML = ''
+
+//         var isValid = true
+
+//         if (id === "") {
+//             idError.innerHTML = "Veuillez saisir un ID.";
+//             isValid = false
+//         }
+// }
+
+// ValidationForm ()
